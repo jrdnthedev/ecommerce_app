2d420bfb81a830df1057b8378336c36a75679322
smooooky <75350537+smooooky@users.noreply.github.com>
2023-09-10 11:47:09 -0400

    using state to manage passed down props

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 
 export default class Product extends Component {
+  state = {
+    product: this.props.prod,
+  };
   render() {
     return (
       <div className="col-lg-6">
         <div className="card m-2">
           <div className="card-body">
-            <p className="text-muted">product #{this.props.prod.id}</p>
-            <h5 className="pt-2 border-top">{this.props.prod.productName}</h5>
-            <p>${this.props.prod.price}</p>
+            <p className="text-muted">product #{this.state.product.id}</p>
+            <h5 className="pt-2 border-top">
+              {this.state.product.productName}
+            </h5>
+            <p>${this.state.product.price}</p>
             {/* <p>qunatity {this.props.prod.quantity}</p> */}
           </div>
         </div>
